265c0c8134fcfeb2c88d2e0de629ce1c68a5b8a0
hdoc <blueguitarist1520@gmail.com>
2022-10-29 21:11:54 -0500

    Feat: Add bordered window to 'cmp' completions

--- plugins/nvim-cmp.lua
+++ plugins/nvim-cmp.lua
@@ -1,11 +1,14 @@
 local cmp = require('cmp')
 local lspkind = require('lspkind')
+local cmp_window_bordered = cmp.config.window.bordered()
 
 cmp.setup({
   snippet = {
-    expand = function(args)
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
+    expand = function(args) vim.fn['vsnip#anonymous'](args.body) end,
+  },
+  window = {
+    completion = cmp_window_bordered,
+    documentation = cmp_window_bordered,
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -24,10 +27,7 @@ cmp.setup({
     format = lspkind.cmp_format({
       mode = 'symbol_text',
       maxwidth = 50,
-      before = function (_, vim_item)
-        return vim_item
-      end
-    })
-  }
+      before = function(_, vim_item) return vim_item end,
+    }),
+  },
 })
-
