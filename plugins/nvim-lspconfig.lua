fd12e72dada6d47eef1e329077f1f04c256f4476
hdoc <blueguitarist1520@gmail.com>
2022-10-29 21:17:44 -0500

    Feat: Update lsp servers configurations

--- plugins/nvim-lspconfig.lua
+++ plugins/nvim-lspconfig.lua
@@ -1,5 +1,9 @@
+local merge = require('utils').merge
 local nvim_lsp = require('lspconfig')
-local servers = { 'cssls', 'eslint', 'html', 'tsserver', 'vimls', 'yamlls', 'marksman', 'bashls' }
+local fn = vim.fn
+local api = vim.api
+local diagnostic = vim.diagnostic
+local servers = { 'cssls', 'eslint', 'html', 'jsonls', 'tsserver', 'sumneko_lua', 'vimls', 'yamlls', 'marksman', 'bashls' }
 
 local on_attach = function(_, bufnr)
   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
@@ -25,28 +29,63 @@ local on_attach = function(_, bufnr)
   buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
   buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
   -- buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+  -- Show inline diagnostic automatically in Hover Window
+  api.nvim_create_autocmd('CursorHold', {
+    buffer = bufnr,
+    callback = function()
+      local float_opts = {
+        focusable = false,
+        close_events = { 'BufLeave', 'CursorMoved', 'InsertEnter', 'FocusLost' },
+        border = 'rounded',
+        source = 'always', -- show source in diagnostic popup window
+        prefix = ' ',
+      }
 
-for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
+      if not vim.b.diagnostics_pos then
+        vim.b.diagnostics_pos = { nil, nil }
+      end
+
+      local cursor_pos = api.nvim_win_get_cursor(0)
+      if
+        (cursor_pos[1] ~= vim.b.diagnostics_pos[1] or cursor_pos[2] ~= vim.b.diagnostics_pos[2])
+        and #diagnostic.get() > 0
+      then
+        diagnostic.open_float(nil, float_opts)
+      end
+
+      vim.b.diagnostics_pos = cursor_pos
+    end,
   })
 end
 
--- SETUP LUA
-nvim_lsp.sumneko_lua.setup({
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+-- LSP GENERAL SETTINGS
+local lsp_general_settings = {
   on_attach = on_attach,
   capabilities = capabilities,
+}
+
+-- LUA SETTINGS
+local lua_settings = {
   settings = {
     Lua = {
-      diagnostics = {
-        globals = { 'vim' },
-      },
+      diagnostics = { globals = { 'vim' } },
     },
   },
-})
+}
 
+for _, lsp in ipairs(servers) do
+  if lsp == 'sumneko_lua' then
+    nvim_lsp[lsp].setup(merge(lsp_general_settings, lua_settings))
+  else
+    nvim_lsp[lsp].setup(lsp_general_settings)
+  end
+end
+
+-- Change diagnostic signs
+fn.sign_define('DiagnosticSignError', { text = '✗', texthl = 'DiagnosticSignError' })
+fn.sign_define('DiagnosticSignWarn', { text = '!', texthl = 'DiagnosticSignWarn' })
+fn.sign_define('DiagnosticSignInformation', { text = '', texthl = 'DiagnosticSignInfo' })
+fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
