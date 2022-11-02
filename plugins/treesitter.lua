2c08e23b9dcb7fafeedd4c377063c9a919faaf5b
hdoc <blueguitarist1520@gmail.com>
2022-11-02 18:40:48 -0500

    Feat: Add vim to treesitter parsers list

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,13 +1,23 @@
 require('nvim-treesitter.install').compilers = { 'zig', 'gcc' }
 
-require('nvim-treesitter.configs').setup {
-  ensure_installed = { 'c', 'cpp', 'bash', 'css', 'gitignore', 'html', 'javascript', 'json', 'lua', 'scss', 'typescript', 'yaml' },
-  highlight = {
-    enable = true,
-    disable = { 'vim' },
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+    'c',
+    'cpp',
+    'bash',
+    'css',
+    'gitignore',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'scss',
+    'typescript',
+    'vim',
+    'yaml',
   },
+  highlight = { enable = true },
   indent = { enable = true },
   -- Config for nvim-ts-autotag
   autotag = { enable = true },
-}
-
+})
