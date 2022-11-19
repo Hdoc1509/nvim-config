25a03bbdbb3c5ac2754060929a9d701ef5c41ff7
hdoc <blueguitarist1520@gmail.com>
2022-11-18 22:09:51 -0500

    Feat: Add formatter for 'sh' filetype

--- plugins/formatter.lua
+++ plugins/formatter.lua
@@ -3,13 +3,14 @@ local prettier = require('formatter.defaults.prettier')
 
 formatter.setup {
   filetype = {
-    lua = { require('formatter.filetypes.lua').stylua },
     css = { prettier('css') },
     html = { prettier('html') },
     javascript = { prettier },
     javascriptreact = { prettier },
     json = { prettier('json') },
+    lua = { require('formatter.filetypes.lua').stylua },
     markdown = { prettier('markdown') },
+    sh = { require('formatter.filetypes.sh').shfmt },
     typescript = { prettier('typescript') },
     typescriptreact = { prettier('typescript') },
     yaml = { prettier('yaml') },
