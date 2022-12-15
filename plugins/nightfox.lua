c66e5f62bf84567df9504a4f1b87e9c0cf9fd205
hdoc <blueguitarist1520@gmail.com>
2022-12-14 19:37:21 -0500

    Feat: Add custom colors for all palettes of nightfox theme

--- plugins/nightfox.lua
+++ plugins/nightfox.lua
@@ -9,7 +9,10 @@ require('nightfox').setup({
     all = {
       bg0 = "#101010",
       bg1 = "#030303",
-      bg3 = "#202020"
+      bg3 = "#202020",
+      white = '#ffffff',
+      green = '#00a600',
+      fg1 = '#ffffff',
     }
   }
 })
