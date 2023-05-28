6b3f43d51cd5760ea03f8c64990205f26237ed67
hdoc <blueguitarist1520@gmail.com>
2023-05-28 11:50:13 -0500

    refactor: use default config for `indent-blankline` plugin

--- plugins/indent-blankline.lua
+++ plugins/indent-blankline.lua
@@ -1,8 +1,3 @@
 vim.opt.list = true
 
-require("indent_blankline").setup {
-  show_current_context = true,
-  use_treesitter = true,
-  use_treesitter_scope = true,
-}
-
+require('indent_blankline').setup()
