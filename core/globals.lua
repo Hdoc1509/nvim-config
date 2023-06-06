2bd3d4259aa56641fe433c533265a0ee667a06d6
hdoc <blueguitarist1520@gmail.com>
2023-06-05 21:42:04 -0500

    feat: disable `node` and `python3` providers

--- core/globals.lua
+++ core/globals.lua
@@ -8,8 +8,10 @@ g.loaded_netrw       = 1
 g.loaded_netrwPlugin = 1
 
 -- Disable providers
-g.loaded_ruby_provider = 0
-g.loaded_perl_provider = 0
+g.loaded_ruby_provider    = 0
+g.loaded_perl_provider    = 0
+g.loaded_node_provider    = 0
+g.loaded_python3_provider = 0
 
 -- Mapleader <Leader>
 g.mapleader = ' '
