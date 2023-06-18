953be35fa3819ad74831dc1bff82aa338395d684
hdoc <blueguitarist1520@gmail.com>
2023-06-17 22:33:33 -0500

    refactor: improve logic for change diagnostic signs

--- lua/plugins/lsp/init.lua
+++ lua/plugins/lsp/init.lua
@@ -1,13 +1,14 @@
 local nvim_lsp = require('lspconfig')
-local servers = require("plugins.lsp.servers")
-local fn = vim.fn
+local servers = require('plugins.lsp.servers')
 
 for server, setting in pairs(servers) do
   nvim_lsp[server].setup(setting)
 end
 
 -- Change diagnostic signs
-fn.sign_define('DiagnosticSignError', { text = '✗', texthl = 'DiagnosticSignError' })
-fn.sign_define('DiagnosticSignWarn', { text = '!', texthl = 'DiagnosticSignWarn' })
-fn.sign_define('DiagnosticSignInformation', { text = '', texthl = 'DiagnosticSignInfo' })
-fn.sign_define('DiagnosticSignHint', { text = '', texthl = 'DiagnosticSignHint' })
+local signs = { Error = '✗', Warn = '!', Hint = '', Info = '' }
+
+for type, icon in pairs(signs) do
+  local hl = 'DiagnosticSign' .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
