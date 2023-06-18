72c27700610e227fff66913ddb8fa3590b4bf57f
hdoc <blueguitarist1520@gmail.com>
2023-06-17 22:18:04 -0500

    feat: move settings of lsp servers to another file

---
+++ lua/plugins/lsp/servers.lua
@@ -0,0 +1,68 @@
+local merge = require('utils').merge
+local lsp_attach = require('plugins.lsp.attach')
+
+local lsp = vim.lsp
+
+local handlers = {
+  ['textDocument/hover'] = lsp.with(lsp.handlers.hover, {
+    border = 'rounded',
+  }),
+  ['textDocument/signatureHelp'] = lsp.with(lsp.handlers.signature_help, {
+    border = 'rounded',
+  }),
+}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+capabilities.textDocument.foldingRange = {
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
+}
+
+-- LSP GENERAL SETTINGS
+local default_settings = {
+  on_attach = lsp_attach,
+  capabilities = capabilities,
+  handlers = handlers,
+}
+
+return {
+  ['bashls'] = merge(default_settings, {
+    filetypes = {
+      'sh',
+      'zsh',
+    },
+  }),
+  ['cssls'] = default_settings,
+  ['emmet_ls'] = default_settings,
+  ['eslint'] = default_settings,
+  ['html'] = default_settings,
+  ['jsonls'] = merge(default_settings, {
+    settings = {
+      json = {
+        schemas = require('schemastore').json.schemas({
+          select = { '.eslintrc', 'package.json' },
+        }),
+        validate = { enable = true },
+      },
+    },
+  }),
+  ['lua_ls'] = merge(default_settings, {
+    settings = {
+      Lua = {
+        diagnostics = { globals = { 'vim' } },
+      },
+    },
+  }),
+  ['marksman'] = default_settings,
+  ['tsserver'] = default_settings,
+  ['vimls'] = default_settings,
+  ['yamlls'] = merge(default_settings, {
+    settings = {
+      yaml = {
+        schemas = require('schemastore').yaml.schemas({
+          select = { 'GitHub Workflow' },
+        }),
+      },
+    },
+  }),
+}
