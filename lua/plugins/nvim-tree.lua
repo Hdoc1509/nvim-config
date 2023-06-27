fc9c6c5c5bf5e9a0ab9dffef3e1f0f4234921a70
hdoc <blueguitarist1520@gmail.com>
2023-06-27 18:50:57 -0500

    refactor: remove `nvim-tree` automatic open file upon creation

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,6 @@
 local nmap = require("utils").nmap
 local api = require("nvim-tree.api")
 
--- Automatically open file upon creation
-api.events.subscribe(api.events.Event.FileCreated, function (file)
-  vim.cmd('edit ' .. file.fname)
-end)
-
 require('nvim-tree').setup({
   filters = {
     custom = {
