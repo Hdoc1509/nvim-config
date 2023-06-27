c5980532f233ea5d45ab3fa795e26cf7a4222343
hdoc <blueguitarist1520@gmail.com>
2023-06-27 18:56:35 -0500

    refactor: add custom mappings for `nvim-tree`

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,5 +1,22 @@
-local nmap = require("utils").nmap
-local api = require("nvim-tree.api")
+local nmap = require('utils').nmap
+local api = require('nvim-tree.api')
+
+local open_tab_silent = function(node)
+  api.node.open.tab(node)
+  vim.cmd('tabprev | NvimTreeOpen')
+end
+
+---@type function
+local open_tab_drop = api.node.open.tab_drop
+
+local on_attach = function(bufnr)
+  api.config.mappings.default_on_attach(bufnr)
+
+  nmap('h', api.node.navigate.parent_close, { desc = 'Close directory', buffer = bufnr })
+  nmap('l', open_tab_drop, { desc = 'Open', buffer = bufnr })
+  nmap('T', open_tab_silent, { desc = 'Open in tab silent', buffer = bufnr })
+  nmap('<CR>', open_tab_drop, { desc = 'Open', buffer = bufnr })
+end
 
 require('nvim-tree').setup({
   filters = {
@@ -35,6 +52,7 @@ require('nvim-tree').setup({
   git = {
     ignore = false,
   },
+  on_attach = on_attach,
 })
 
 nmap('<F1>', '<cmd>NvimTreeFindFileToggle<CR>')
