b316c21c14af2a9d0331236a0a36656803b86fb7
hdoc <blueguitarist1520@gmail.com>
2023-06-30 22:51:14 -0500

    refactor: remove unused util `create_buf_nmapper`

--- lua/utils.lua
+++ lua/utils.lua
@@ -19,19 +19,6 @@ M.keymap = function(mode, lhs, rhs, opts)
   vim.keymap.set(mode, lhs, rhs, M.merge(map_opts, opts))
 end
 
----Returns a nmap creator for the specified buffer
----@param bufnr number
-M.create_buf_nmapper = function (bufnr)
-  ---Add a buffer nmap
-  ---@param lhs string left-hand side of mapping
-  ---@param rhs string right-hand side of mapping
-  ---@param opts? table aditional options
-  return function(lhs,rhs, opts)
-    opts = opts or {}
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', lhs, rhs, M.merge(map_opts, opts))
-  end
-end
-
 M.map_notify = function(message)
   vim.notify(message, 'info', {
     title = 'Mapping',
