bb04578856e04f710503b2928bcced2666af37c5
hdoc <blueguitarist1520@gmail.com>
2023-07-01 23:35:14 -0500

    refactor: move mappings for `vim-vsnip` to `cmp` config file

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -2,6 +2,10 @@ local cmp = require('cmp')
 local lspkind = require('lspkind')
 local cmp_window_bordered = cmp.config.window.bordered()
 
+local feedkey = function(key, mode)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+end
+
 cmp.setup({
   snippet = {
     expand = function(args)
@@ -13,6 +17,24 @@ cmp.setup({
     documentation = cmp_window_bordered,
   },
   mapping = cmp.mapping.preset.insert({
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif vim.fn['vsnip#jumpable'](1) == 1 then
+        feedkey('<Plug>(vsnip-jump-next)', '')
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif vim.fn['vsnip#jumpable'](1) == 1 then
+        feedkey('<Plug>(vsnip-jump-prev)', '')
+      else
+        fallback()
+      end
+    end,
     ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item()),
     ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item()),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
--- lua/plugins/vim-vsnip.lua
+++ lua/plugins/vim-vsnip.lua
@@ -1,15 +1,3 @@
-local keymap = require('utils').keymap
-
--- Jump forward
-keymap({ 'i', 's' }, '<Tab>', function()
-  return vim.fn['vsnip#jumpable'](1) == 1 and '<Plug>(vsnip-jump-next)' or '<Tab>'
-end, { expr = true })
-
--- Jump backward
-keymap({ 'i', 's' }, '<S-Tab>', function()
-  return vim.fn['vsnip#jumpable'](-1) == 1 and '<Plug>(vsnip-jump-prev)' or '<S-Tab>'
-end, { expr = true })
-
 vim.g.vsnip_filetypes = {
   typescript = { 'javascript' },
 }
