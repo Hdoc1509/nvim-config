8c5d4789ef3bb5e48fe6b021b9a6412e815c62f9
hdoc <blueguitarist1520@gmail.com>
2023-07-01 21:38:12 -0500

    refactor: use `open.drop` for open files with `nvim-tree`

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -7,15 +7,15 @@ local open_tab_silent = function(node)
 end
 
 ---@type function
-local open_tab_drop = api.node.open.tab_drop
+local open_drop = api.node.open.drop
 
 local on_attach = function(bufnr)
   api.config.mappings.default_on_attach(bufnr)
 
   nmap('h', api.node.navigate.parent_close, { desc = 'Close directory', buffer = bufnr })
-  nmap('l', open_tab_drop, { desc = 'Open', buffer = bufnr })
+  nmap('l', open_drop, { desc = 'Open', buffer = bufnr })
   nmap('T', open_tab_silent, { desc = 'Open in tab silent', buffer = bufnr })
-  nmap('<CR>', open_tab_drop, { desc = 'Open', buffer = bufnr })
+  nmap('<CR>', open_drop, { desc = 'Open', buffer = bufnr })
 end
 
 require('nvim-tree').setup({
