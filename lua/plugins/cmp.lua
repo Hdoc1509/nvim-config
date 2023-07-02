c00b074ce345db7925d0446d4f2b57800fc0d41f
hdoc <blueguitarist1520@gmail.com>
2023-07-02 18:13:57 -0500

    refactor: remove use of `<Tab>` and `<S-Tab>` for select completion items

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -18,18 +18,14 @@ cmp.setup({
   },
   mapping = cmp.mapping.preset.insert({
     ['<Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif vim.fn['vsnip#jumpable'](1) == 1 then
+      if vim.fn['vsnip#jumpable'](1) == 1 then
         feedkey('<Plug>(vsnip-jump-next)', '')
       else
         fallback()
       end
     end,
     ['<S-Tab>'] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif vim.fn['vsnip#jumpable'](1) == 1 then
+      if vim.fn['vsnip#jumpable'](1) == 1 then
         feedkey('<Plug>(vsnip-jump-prev)', '')
       else
         fallback()
