6cbec293f81f66422e077c82f9bab81f2b62e16d
hdoc <blueguitarist1520@gmail.com>
2023-07-28 12:55:13 -0500

    fix: add missing mode mappings for select next/prev item of completion

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -17,20 +17,20 @@ cmp.setup({
     documentation = cmp_window_bordered,
   },
   mapping = cmp.mapping.preset.insert({
-    ['<Tab>'] = function(fallback)
+    ['<Tab>'] = cmp.mapping(function(fallback)
       if vim.fn['vsnip#jumpable'](1) == 1 then
         feedkey('<Plug>(vsnip-jump-next)', '')
       else
         fallback()
       end
-    end,
-    ['<S-Tab>'] = function(fallback)
-      if vim.fn['vsnip#jumpable'](1) == 1 then
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if vim.fn['vsnip#jumpable'](-1) == 1 then
         feedkey('<Plug>(vsnip-jump-prev)', '')
       else
         fallback()
       end
-    end,
+    end, { 'i', 's' }),
     ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item()),
     ['<C-k>'] = cmp.mapping(cmp.mapping.select_prev_item()),
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
