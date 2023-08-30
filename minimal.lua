b63be3ea464e3ded09e0d170e411fe9b84603c74
hdoc <hector.ochoa.dev@gmail.com>
2023-08-30 17:48:15 -0500

    feat: liad mappings in minimal config

--- minimal.lua
+++ minimal.lua
@@ -1,8 +1,9 @@
 require('globals')
 require('options')
-require("lazy_bootstrap")
+require('lazy_bootstrap')
 require('lazy').setup({
   { import = 'plugins.lightline' },
   { import = 'plugins.nightfox' },
   { 'jiangmiao/auto-pairs' },
 })
+require('maps')
