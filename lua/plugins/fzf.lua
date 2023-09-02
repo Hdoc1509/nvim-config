a74d717aadc349f0ac2805391e444786f2d20301
hdoc <hector.ochoa.dev@gmail.com>
2023-09-02 13:00:46 -0500

    feat: use fzf options if not set by use shell

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,23 +1,26 @@
 local config = function()
-  local nmap = require('utils').nmap
+  -- Use these options if fzf options are not set by user shell
+  if not vim.fn.empty(vim.env.FZF_DEFAULT_COMMAND) then
+    local nmap = require('utils').nmap
 
-  vim.cmd([[
-  let exclude = {
-    \ 'dir' : ['dist', 'node_modules', '.git'],
-    \ 'file': ['*.exe', '*.dll', '*.so', '*.o', '*.pyc', '*.jpg', '*.png',
-    \ '*.gif', '*.svg', '*.ico', '*.db', '*.tgz', '*.tar.gz', '*.gz',
-    \ '*.zip', '*.bin', '*.pptx', '*.xlsx', '*.docx', '*.pdf', '*.tmp',
-    \ '*.wmv', '*.mkv', '*.mp4', '*.rmvb', '*.ttf', '*.ttc', '*.otf',
-    \ '*.mp3', '*.aac', 'package-lock.json', 'pnpm-lock.yaml']
-    \ }
+    vim.cmd([[
+    let exclude = {
+      \ 'dir' : ['dist', 'node_modules', '.git'],
+      \ 'file': ['*.exe', '*.dll', '*.so', '*.o', '*.pyc', '*.jpg', '*.png',
+      \ '*.gif', '*.svg', '*.ico', '*.db', '*.tgz', '*.tar.gz', '*.gz',
+      \ '*.zip', '*.bin', '*.pptx', '*.xlsx', '*.docx', '*.pdf', '*.tmp',
+      \ '*.wmv', '*.mkv', '*.mp4', '*.rmvb', '*.ttf', '*.ttc', '*.otf',
+      \ '*.mp3', '*.aac', 'package-lock.json', 'pnpm-lock.yaml']
+      \ }
 
-  let g:fzf_exclude = join(exclude.file, ',') . ',' . join(exclude.dir, ',')
-  ]])
+    let g:fzf_exclude = join(exclude.file, ',') . ',' . join(exclude.dir, ',')
+    ]])
 
-  vim.env.FZF_DEFAULT_COMMAND = "fd --type f --hidden --follow --no-ignore --exclude={'" .. vim.g.fzf_exclude .. "'}"
-  vim.env.FZF_DEFAULT_OPTS = '--layout=reverse'
+    vim.env.FZF_DEFAULT_COMMAND = "fd --type f --hidden --follow --no-ignore --exclude={'" .. vim.g.fzf_exclude .. "'}"
+    vim.env.FZF_DEFAULT_OPTS = '--layout=reverse'
 
-  nmap('<Leader>e', '<cmd>FZF<CR>')
+    nmap('<Leader>e', '<cmd>FZF<CR>')
+  end
 end
 
 return {
