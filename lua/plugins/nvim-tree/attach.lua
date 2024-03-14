b13940e8a80ae0275be1daf67dfe404d14e972de
hdoc <hector.ochoa.dev@gmail.com>
2024-03-13 21:46:43 -0500

    feat(nvim-tree): add mapping to copy file

--- lua/plugins/nvim-tree/attach.lua
+++ lua/plugins/nvim-tree/attach.lua
@@ -7,6 +7,16 @@ local open_tab_silent = function(node)
   vim.cmd('tabprev | NvimTreeOpen')
 end
 
+local copy_file_to = function()
+  local node = api.tree.get_node_under_cursor()
+  local file_src = node.absolute_path
+  local file_out = vim.fn.input('Copy to: ', file_src, 'file')
+  local dir = vim.fn.fnamemodify(file_out, ':h')
+
+  vim.fn.system({ 'mkdir', '-p', dir })
+  vim.fn.system({ 'cp', '-R', file_src, file_out })
+end
+
 ---@type function
 local open_drop = api.node.open.drop
 
@@ -17,4 +27,5 @@ return function(bufnr)
   nmap('l', open_drop, { desc = 'Open', buffer = bufnr })
   nmap('T', open_tab_silent, { desc = 'Open in tab silent', buffer = bufnr })
   nmap('<CR>', open_drop, { desc = 'Open', buffer = bufnr })
+  nmap('c', copy_file_to, { desc = 'Copy file to', buffer = bufnr })
 end
