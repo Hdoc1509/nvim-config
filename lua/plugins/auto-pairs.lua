af98df30b1b48da265738767a613ff3a3e35645a
hdoc <hector.ochoa.dev@gmail.com>
2024-03-23 19:40:52 -0500

    feat(auto-pairs): add config file

---
+++ lua/plugins/auto-pairs.lua
@@ -0,0 +1,37 @@
+local autocmd = require('utils').autocmd
+
+local function config()
+  -- Add auto-pairs for HTML comment
+  autocmd('FileType', {
+    pattern = { 'html', 'markdown' },
+    callback = function()
+      vim.cmd("let b:AutoPairs = AutoPairsDefine({'<!--' : '-->'})")
+    end,
+  })
+
+  -- Add auto-pairs for React.Fragment
+  autocmd('FileType', {
+    pattern = { 'javascriptreact', 'typescriptreact' },
+    callback = function()
+      vim.cmd("let b:AutoPairs = AutoPairsDefine({'<>':'</>'})")
+    end,
+  })
+
+  -- Add auto-pairs for ejs
+  autocmd('FileType', {
+    pattern = 'ejs',
+    callback = function(ev)
+      vim.b[ev.buf].AutoPairs = vim.fn['AutoPairsDefine']({
+        ['<%'] = '%>',
+        ['<%='] = '%>',
+        ['<%-'] = '%>',
+        ['<%#'] = '%>',
+      })
+    end,
+  })
+end
+
+return {
+  'jiangmiao/auto-pairs',
+  config = config,
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,5 +1,4 @@
 return {
-  'jiangmiao/auto-pairs',
   'matze/vim-move',
   'chrisbra/unicode.vim',
   'equalsraf/neovim-gui-shim',
