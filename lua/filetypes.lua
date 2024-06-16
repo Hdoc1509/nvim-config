a2881210fbd6bbe25c7501e26affaad3a74081ff
hdoc <hector.ochoa.dev@gmail.com>
2024-06-16 18:24:36 -0500

    refactor: remove pattern match to set `ejs` filetype

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -4,7 +4,4 @@ vim.filetype.add({
     -- taken and adapted from https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/#supporting-mdx-files
     mdx = 'markdown.mdx',
   },
-  pattern = {
-    ['.*.ejs.*'] = { 'ejs', { priority = 10 } },
-  },
 })
