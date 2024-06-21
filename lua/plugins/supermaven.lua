5420a4846c310bdbec06116e587179cb92017357
hdoc <hector.ochoa.dev@gmail.com>
2024-06-21 17:40:14 -0500

    feat(supermaven): use same keymaps as `codeium`

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,6 +1,11 @@
 return {
   'supermaven-inc/supermaven-nvim',
   config = function()
-    require('supermaven-nvim').setup({})
+    require('supermaven-nvim').setup({
+      keymaps = {
+        accept_suggestion = '<C-a>',
+        clear_suggestion = '<C-q>',
+      },
+    })
   end,
 }
