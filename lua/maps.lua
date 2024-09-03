a136f2e1cab647e5b2517e4bb5b950001da5f5db
hdoc <hector.ochoa.dev@gmail.com>
2024-09-03 17:19:26 -0500

    feat(maps): create necessary directories when saving file

--- lua/maps.lua
+++ lua/maps.lua
@@ -4,7 +4,7 @@ local nmap = utils.nmap
 local keymap = utils.keymap
 
 -- Save and quit files
-nmap('<leader>w', '<cmd>update<cr>', { desc = 'Save file' })
+nmap('<leader>w', '<cmd>update ++p<cr>', { desc = 'Save file' })
 nmap('<leader>W', '<cmd>wall<cr>', { desc = 'Save all files' })
 nmap('<leader>q', '<cmd>xit<cr>', { desc = 'Save and close file' })
 nmap('<leader>Q', '<cmd>xall<cr>', { desc = 'Save all files and close Neovim' })
