53c56c03c09ab1101760baad8705f994898efd2d
hdoc <hector.ochoa.dev@gmail.com>
2024-09-21 21:29:39 -0500

    feat(treesitter): add directive to inject any language in ejs files

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,3 +1,22 @@
+local ext_to_parser = {
+  ['ts'] = 'typescript',
+  ['js'] = 'javascript',
+  ['jsx'] = 'javascriptreact',
+  ['md'] = 'markdown',
+  ['svg'] = 'html',
+}
+
+local directives = {
+  -- https://github.com/nvim-treesitter/nvim-treesitter/discussions/1917#discussioncomment-10714144
+  ['inject-ejs-tmpl!'] = function(_, _, bufnr, _, metadata)
+    local fname = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
+    -- files ending in .{ext}.ejs
+    local _, _, ext, _ = string.find(fname, '.*%.(%a+)(%.%a+)')
+
+    metadata['injection.language'] = ext_to_parser[ext] or ext
+  end,
+}
+
 local config = function()
   require('nvim-treesitter.install').prefer_git = false
   require('nvim-treesitter.install').compilers = { 'zig', 'gcc' }
@@ -32,9 +51,15 @@ local config = function()
     autotag = { enable = true }, -- nvim-ts-autotag
   })
 
+  -- REGISTER LANGUAGES
   vim.treesitter.language.register('embedded_template', 'ejs')
   -- taken and adapted from https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/#supporting-mdx-files
   vim.treesitter.language.register('markdown', 'markdown.mdx')
+
+  -- DIRECTIVES
+  for directive, callback in pairs(directives) do
+    vim.treesitter.query.add_directive(directive, callback, {})
+  end
 end
 
 return {
---
+++ queries/embedded_template/injections.scm
@@ -0,0 +1,3 @@
+((content) @injection.content
+  (#inject-ejs-tmpl!)
+  (#set! injection.combined))
