a94a4957e44fd99fba69fe0e7e63e53710a19d73
hdoc <hector.ochoa.dev@gmail.com>
2024-09-21 21:13:02 -0500

    feat(ft): treat `svg` files as `html`

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -3,5 +3,6 @@ vim.filetype.add({
     ejs = 'ejs',
     -- taken and adapted from https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/#supporting-mdx-files
     mdx = 'markdown.mdx',
+    svg = 'html',
   },
 })
