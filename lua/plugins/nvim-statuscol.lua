160b4b13efc3a7d789a2fe8a608a0d2708443c45
hdoc <hector.ochoa.dev@gmail.com>
2024-09-23 12:23:33 -0500

    refactor(statuscol): reorder statuscol content

--- lua/plugins/nvim-statuscol.lua
+++ lua/plugins/nvim-statuscol.lua
@@ -4,9 +4,9 @@ local config = function()
   require('statuscol').setup({
     relculright = true,
     segments = {
-      { text = { builtin.foldfunc }, click = 'v:lua.ScFa' },
       { text = { '%s' }, click = 'v:lua.ScSa' },
-      { text = { builtin.lnumfunc, ' ' }, click = 'v:lua.ScLa' },
+      { text = { builtin.lnumfunc }, click = 'v:lua.ScLa' },
+      { text = { builtin.foldfunc, ' ' }, click = 'v:lua.ScFa' },
     },
   })
 end
