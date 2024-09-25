4961056f87e2a6f58f120fb63ffc90259f7e8865
hdoc <hector.ochoa.dev@gmail.com>
2024-09-25 17:22:29 -0500

    feat(treesitter): add more extensions to ext-to-parser.lua
    - `cjs`
    - `mjs`

--- lua/plugins/treesitter/ext-to-parser.lua
+++ lua/plugins/treesitter/ext-to-parser.lua
@@ -2,6 +2,8 @@ return {
   ['ts'] = 'typescript',
   ['js'] = 'javascript',
   ['jsx'] = 'javascript',
+  ['cjs'] = 'javascript',
+  ['mjs'] = 'javascript',
   ['md'] = 'markdown',
   ['svg'] = 'html',
 }
