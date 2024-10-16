137535a2245d44559102d083a46db68ca3ea78b6
hdoc <hector.ochoa.dev@gmail.com>
2024-10-16 18:25:02 -0500

    feat(lint): add list of ingored files

--- lua/plugins/lint.lua
+++ lua/plugins/lint.lua
@@ -2,12 +2,26 @@ local config = function()
   local lint = require('lint')
   local autocmd = require('utils').autocmd
 
+  local ignore_files = {
+    'pnpm-lock.yaml',
+    'pnpm-workspace.yaml',
+  }
+
   lint.linters_by_ft = {
     yaml = { 'actionlint' },
   }
 
   autocmd('BufWritePost', {
     callback = function()
+      local bufnr = vim.fn.bufnr('%')
+      local filename = vim.fs.basename(vim.api.nvim_buf_get_name(bufnr))
+
+      if vim.tbl_contains(ignore_files, filename) then
+        return
+      end
+
+      print('linting ' .. filename)
+
       lint.try_lint()
     end,
   })
