c9cd186b192af1c13c30a4244d919858fc34ebe0
hdoc <hector.ochoa.dev@gmail.com>
2024-12-23 17:17:29 -0500

    feat(fugitive): add more mappings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,6 +1,9 @@
 local config = function()
   local nmap = require('utils').nmap
 
+  nmap('<leader>G', '<cmd>Git<cr>', { desc = 'Git summary window' })
+  nmap('<leader>vG', '<cmd>vert Git<cr>', { desc = 'Git summary window in vertical split' })
+  nmap('<leader>tG', '<cmd>tabnew | Git<cr>', { desc = 'Git summary window in new tab' })
   nmap('<leader>gaa', '<cmd>Git add --all<cr>', { desc = 'Git add all' })
   nmap('<leader>gd', '<cmd>Git diff<cr>', { desc = 'Git diff' })
   nmap('<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
