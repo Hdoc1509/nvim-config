2afc82983355415f5f3ced245386e5fc2e0a6636
hdoc <hector.ochoa.dev@gmail.com>
2024-12-31 11:43:02 -0500

    feat(filetypes): add support for `.mcmeta` files

--- lua/filetypes.lua
+++ lua/filetypes.lua
@@ -4,5 +4,6 @@ vim.filetype.add({
     -- taken and adapted from https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/#supporting-mdx-files
     mdx = 'markdown.mdx',
     svg = 'html',
+    mcmeta = 'json'
   },
 })
