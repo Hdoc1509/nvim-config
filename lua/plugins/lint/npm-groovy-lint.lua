a190482f99fc288b62ac844701cfb241bdcddd14
hdoc <hector.ochoa.dev@gmail.com>
2024-12-31 12:22:40 -0500

    feat(lint): ignore diagnostcs for comments in `groovy` files

--- lua/plugins/lint/npm-groovy-lint.lua
+++ lua/plugins/lint/npm-groovy-lint.lua
@@ -19,6 +19,13 @@ return {
     local diagnostics = {}
 
     for _, item in ipairs(decoded.files[buf_name].errors) do
+      local buf_line = vim.api.nvim_buf_get_lines(bufnr, item.line - 1, item.line, false)[1]
+      local is_comment = string.match(buf_line, '%s*//') ~= nil
+
+      if is_comment then
+        goto continue
+      end
+
       local diagnostic = {
         lnum = item.line - 1,
         end_lnum = item.line - 1,
@@ -34,6 +41,8 @@ return {
       end
 
       table.insert(diagnostics, diagnostic)
+
+      ::continue::
     end
 
     return diagnostics
