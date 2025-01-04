71766778e9c1edae757ab9a10d0da3bf42b6cc90
hdoc <hector.ochoa.dev@gmail.com>
2025-01-04 18:55:39 -0500

    chore: update `fidget.nvim`

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,13 +1,12 @@
 return {
   'j-hui/fidget.nvim',
-  tag = 'legacy',
   event = 'VeryLazy',
   opts = {
-    text = {
-      spinner = 'dots',
-    },
-    window = {
-      blend = 0,
+    notification = {
+      window = {
+        border = 'rounded',
+        winblend = 0,
+      },
     },
   },
 }
