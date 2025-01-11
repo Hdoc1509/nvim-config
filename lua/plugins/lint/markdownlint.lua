fe776003272e9124f908acda27eb7f1f14aaca2f
hdoc <hector.ochoa.dev@gmail.com>
2025-01-11 18:26:52 -0500

    refactor(lint): move filter logic for markdownlint to a separate function

--- lua/plugins/lint/markdownlint.lua
+++ lua/plugins/lint/markdownlint.lua
@@ -11,33 +11,26 @@ local default_parser = lint_parser.from_errorformat(efm, {
   severity = vim.diagnostic.severity.WARN,
 })
 
----@param message string
-local function is_message_to_ignore(message)
-  return string.match(message, 'MD0[23]4') ~= nil
+local function is_diagnostic_ignored(bufnr, diagnostic)
+  local buf_name = vim.api.nvim_buf_get_name(bufnr)
+  local message = diagnostic.message
+  local line_number = diagnostic.lnum
+  local buf_line = vim.api.nvim_buf_get_lines(bufnr, line_number, line_number + 1, false)[1]
+
+  return (string.match(buf_name, 'TODO.md$') ~= nil and string.match(message, 'MD0[23]4') ~= nil)
+    or (string.match(buf_line, '^%|') ~= nil and string.match(message, 'MD013') ~= nil)
 end
 
 return merge(default_markdownlint, {
   ---@type lint.parse
   parser = function(output, bufnr, linter_cwd)
     local all_diagnostics = default_parser(output, bufnr, linter_cwd)
-    local buf_name = vim.api.nvim_buf_get_name(bufnr)
 
     -- NOTE: can I use vim.fn.filter() instead of this loop?
     local idx = 1
 
     while idx <= #all_diagnostics do
-      ---@type string
-      ---@diagnostic disable-next-line: undefined-field
-      local message = all_diagnostics[idx].message
-      ---@type integer
-      ---@diagnostic disable-next-line: undefined-field
-      local line_number = all_diagnostics[idx].lnum
-      local buf_line = vim.api.nvim_buf_get_lines(bufnr, line_number, line_number + 1, false)[1]
-
-      if
-        (string.match(buf_name, 'TODO.md$') ~= nil and is_message_to_ignore(message))
-        or (string.match(buf_line, '^%|') ~= nil and string.match(message, 'MD013') ~= nil)
-      then
+      if is_diagnostic_ignored(bufnr, all_diagnostics[idx]) then
         table.remove(all_diagnostics, idx)
       else
         idx = idx + 1
