dfb0142b95bcea637f1b0c20439f09fe29f3ae3f
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-02-23 18:12:23 -0500

    refactor(icons): access folder icon config values only once

--- lua/icons/folder/init.lua
+++ lua/icons/folder/init.lua
@@ -8,12 +8,14 @@ local folder_icons_config = require('icons.folder.configs')
 local folder_icons = {}
 
 for config_name, config in pairs(folder_icons_config) do
+  local icon, hl = config.icon, config.hl
+
   if config.folder_names ~= nil then
     for _, folder_name in ipairs(config.folder_names) do
-      folder_icons[folder_name] = { icon = config.icon, hl = config.hl }
+      folder_icons[folder_name] = { icon = icon, hl = hl }
     end
   else
-    folder_icons[config_name] = { icon = config.icon, hl = config.hl }
+    folder_icons[config_name] = { icon = icon, hl = hl }
   end
 end
 
