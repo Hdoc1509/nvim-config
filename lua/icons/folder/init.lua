f8d680276fa7b885ea836d19b9b02ad5b3a4410b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-02-23 17:53:40 -0500

    refactor(icons): simplify generation of table of folder icons
    remove `set_config()` util from icons/folder/init.lua

--- lua/icons/folder/init.lua
+++ lua/icons/folder/init.lua
@@ -2,19 +2,13 @@ local folder_icons_config = require('icons.folder.configs')
 
 local folder_icons = {}
 
----@param config_name string
----@param config FolderIconConfig
-local function set_config(config_name, config)
-  folder_icons[config_name] = { icon = config.icon, hl = config.hl }
-end
-
 for config_name, config in pairs(folder_icons_config) do
   if config.folder_names ~= nil then
     for _, folder_name in ipairs(config.folder_names) do
-      set_config(folder_name, config)
+      folder_icons[folder_name] = { icon = config.icon, hl = config.hl }
     end
   else
-    set_config(config_name, config)
+    folder_icons[config_name] = { icon = config.icon, hl = config.hl }
   end
 end
 
