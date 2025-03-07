2e4b70ae0f1912e042ef41e9c909f643a7751dc3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-03-06 21:12:22 -0500

    feat(treesitter): use `bash` parser for `zsh` files

--- lua/plugins/treesitter/register.lua
+++ lua/plugins/treesitter/register.lua
@@ -1,6 +1,7 @@
 -- table[parser_name] = filetype
 return {
-  ['embedded_template'] = 'ejs',
+  bash = 'zsh',
+  embedded_template = 'ejs',
   -- https://phelipetls.github.io/posts/mdx-syntax-highlight-treesitter-nvim/#supporting-mdx-files
-  ['markdown'] = 'markdown.mdx',
+  markdown = 'markdown.mdx',
 }
