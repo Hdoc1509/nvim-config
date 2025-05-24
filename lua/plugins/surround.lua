d19df40728171e7b16ae692cd1496db9a8ecfeaf
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-05-24 18:41:09 -0500

    docs(nvim-surround): add todo about buffer local mappings

--- lua/plugins/surround.lua
+++ lua/plugins/surround.lua
@@ -3,4 +3,6 @@ return {
   config = true,
   event = 'VeryLazy',
   version = '*',
+  -- TODO: set buffer local setup
+  -- https://github.com/kylechui/nvim-surround/issues/7#issuecomment-1178383628
 }
