23af4ef342dcee6ab503f315bf04fe6c271e6e3b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-12 11:58:39 -0500

    feat(indent-blankline): customize `scope` option

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,9 +3,30 @@ vim.opt.listchars = 'trail:·'
 
 return {
   'lukas-reineke/indent-blankline.nvim',
+  dependencies = { 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim' },
   main = 'ibl',
   opts = {
     indent = { char = '│' },
-    scope = { enabled = false },
+    scope = {
+      show_start = false,
+      show_end = false,
+      highlight = {
+        'RainbowDelimiterRed',
+        'RainbowDelimiterYellow',
+        'RainbowDelimiterBlue',
+        'RainbowDelimiterOrange',
+        'RainbowDelimiterGreen',
+        'RainbowDelimiterViolet',
+        'RainbowDelimiterCyan',
+      },
+    },
   },
+  config = function(_, opts)
+    local hooks = require('ibl.hooks')
+
+    require('ibl').setup(opts)
+
+    -- NOTE: updates colors once colorscheme is loaded
+    hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+  end,
 }
