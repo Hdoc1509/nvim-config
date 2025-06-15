3a67aecbd15afc8b1c3e077f8fab7674acd96ceb
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-15 16:45:42 -0500

    refactor(lazy): increase size of ui

--- init.lua
+++ init.lua
@@ -7,6 +7,12 @@ require('lazy').setup('plugins', {
   install = {
     colorscheme = { 'carbonfox' },
   },
+  ui = {
+    size = {
+      width = 1,
+      height = 0.9,
+    },
+  },
 })
 require('maps')
 require('diagnostic')
