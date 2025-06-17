1f7c0b6006904ce4513b5d3669ea1edb10f48506
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-17 18:36:31 -0500

    feat(icons): add `cmd` file icon

--- lua/icons/files/extension.lua
+++ lua/icons/files/extension.lua
@@ -5,4 +5,5 @@ return {
   astro = { icon = '', color = COLORS['deep-purple-A200'], name = 'Astro' },
   -- TODO: add color to COLORS file: cyan-gray-900
   jq = { icon = '', color = '#4D5A5E', name = 'JQ' },
+  cmd = { icon = '', color = '#c1f12e', name = 'CMD' },
 }
