d4b21a004f5e639a1bbdbe3b8759a615571c7c08
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-17 18:36:53 -0500

    feat(devicons): add filetype icons for `powershell` and `cmd`

--- lua/plugins/web-devicons.lua
+++ lua/plugins/web-devicons.lua
@@ -5,12 +5,18 @@ return {
     { 'Hdoc1509/hygen.nvim', tag = 'v0.2.0' },
   },
   config = function()
+    local devicons = require('nvim-web-devicons')
     local ICONS = require('icons')
 
-    require('nvim-web-devicons').setup({
+    devicons.setup({
       override_by_filename = ICONS.file.name,
       override_by_extension = ICONS.file.extension,
     })
+
     require('hygen.web-devicons').setup()
+    devicons.set_icon_by_filetype({
+      powershell = 'ps1',
+      cmd = 'bat',
+    })
   end,
 }
