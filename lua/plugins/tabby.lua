cea23b5c8ab53fb27a1a2823b0fe9f01c34a210e
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-18 11:09:56 -0500

    chore: update `tabby.nvim`

--- lua/plugins/tabby.lua
+++ lua/plugins/tabby.lua
@@ -6,27 +6,30 @@ local config = function()
     win = 'TabLine',
   }
 
-  require('tabby.tabline').set(function(line)
-    return {
-      line.tabs().foreach(function(tab)
-        local hl = tab.is_current() and theme.current_tab or theme.tab
-        local window = tab.current_win()
+  require('tabby').setup({
+    line = function(line)
+      return {
+        line.tabs().foreach(function(tab)
+          local hl = tab.is_current() and theme.current_tab or theme.tab
+          local window = tab.current_win()
 
-        return {
-          '',
-          tab.number(),
-          window.file_icon(), -- icon of current window
-          window.buf_name(), -- no number of windows in tab
-          -- tab.name(),
-          hl = hl,
-          margin = ' ',
-          '',
-        }
-      end),
-      line.spacer(),
-      hl = theme.fill,
-    }
-  end)
+          return {
+            ' ',
+            tab.number(),
+            ' ',
+            window.file_icon(), -- icon of current window
+            ' ',
+            window.buf_name(), -- no number of windows in tab
+            -- tab.name(),
+            hl = hl,
+            ' ',
+          }
+        end),
+        line.spacer(),
+        hl = theme.fill,
+      }
+    end,
+  })
 end
 
 return {
