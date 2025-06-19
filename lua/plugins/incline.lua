726ff489c128f27aa5edd4ecf996d5250dc17915
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-19 12:14:30 -0500

    feat(incline): sync filename colors with lightline mode colors

--- lua/plugins/incline.lua
+++ lua/plugins/incline.lua
@@ -1,6 +1,7 @@
 local config = function()
   local helpers = require('incline.helpers')
   local devicons = require('nvim-web-devicons')
+  local lightline_palette = vim.fn['lightline#palette']()
 
   require('incline').setup({
     window = {
@@ -8,6 +9,25 @@ local config = function()
       margin = { horizontal = 0 },
     },
     render = function(props)
+      local file_fg = ''
+      local file_bg = ''
+
+      if not props.focused then
+        file_fg = lightline_palette.inactive.left[1][1]
+        file_bg = lightline_palette.inactive.left[1][2]
+      else
+        local mode = string.lower(vim.fn['lightline#mode']())
+
+        if mode == 'command' or mode == 'terminal' then
+          mode = 'normal'
+        elseif mode == 'select' or string.match(mode, '^[sv]%-') ~= nil then
+          mode = 'visual'
+        end
+
+        file_fg = lightline_palette[mode].left[1][1]
+        file_bg = lightline_palette[mode].left[1][2]
+      end
+
       local filename = vim.fn.fnamemodify(vim.api.nvim_buf_get_name(props.buf), ':t')
 
       if filename == '' then
@@ -32,7 +52,8 @@ local config = function()
         ' ',
         { filename, gui = modified and 'bold,italic' or 'bold' },
         ' ',
-        guibg = '#44406e',
+        guibg = file_bg,
+        guifg = file_fg,
       }
     end,
   })
@@ -40,6 +61,9 @@ end
 
 return {
   'b0o/incline.nvim',
+  dependencies = {
+    'itchyny/lightline.vim',
+  },
   config = config,
   event = 'VeryLazy',
 }
