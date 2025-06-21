f52a6c75b7e0e2240c601b4bafe84b215fcfd900
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-21 18:13:45 -0500

    feat(plugins): add `inc-rename.nvim` for nvim-0.10

---
+++ lua/plugins/inc-rename.lua
@@ -0,0 +1,10 @@
+return {
+  'smjonas/inc-rename.nvim',
+  opts = {},
+  keys = {
+    { '<leader>rn', ':IncRename ', mode = { 'n' } },
+  },
+  -- NOTE: until update to nvim-0.10
+  -- TODO: add to docs once updated to nvim-0.10
+  enabled = false,
+}
