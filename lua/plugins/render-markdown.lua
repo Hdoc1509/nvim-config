92472a9bad7714843198e01dc606ed9ff675d44a
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-22 12:47:45 -0500

    chore(render-markdown): downgrade to previous installed version
    last update broke table rendering

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,20 +1,16 @@
+local config = function()
+  local nmap = require('utils').nmap
+
+  require('render-markdown').setup({
+    headings = { '◉ ', '○ ', '✸ ', '✿ ' },
+  })
+
+  nmap('<leader>mt', '<cmd>RenderMarkdownToggle<cr>', { desc = 'Toggle Markdown renderer' })
+end
+
 return {
   'MeanderingProgrammer/markdown.nvim',
   name = 'render-markdown',
   dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  opts = {
-    heading = {
-      icons = { '◉ ', '○ ', '✸ ', '✿ ' },
-      sign = false,
-      -- position = 'inline', FIX: breaks custom icons. works from nvim-0.10
-      width = 'block',
-      min_width = 79,
-    },
-    code = {
-      -- position = 'left', FIX: not working. works from nvim-0.10
-      sign = false,
-      width = 'block',
-      below = '',
-    },
-  },
+  config = config,
 }
