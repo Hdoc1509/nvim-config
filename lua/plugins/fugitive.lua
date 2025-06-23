63f39a93a28b66ca02d54cc273d881f58ca15448
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-23 11:52:43 -0500

    refactor(fugitive): simplify keymaps definition

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,22 +1,18 @@
-local config = function()
-  local nmap = require('utils').nmap
-
-  nmap('<leader>gG', '<cmd>Git<cr>', { desc = 'Git summary window' })
-  nmap('<leader>gV', '<cmd>vert Git<cr>', { desc = 'Git summary window in vertical split' })
-  nmap('<leader>gT', '<cmd>tabnew | Git<cr><c-w>o<cr>', { desc = 'Git summary window in new tab' })
-  nmap('<leader>gaa', '<cmd>Git add --all<cr>', { desc = 'Git add all' })
-  nmap('<leader>gd', '<cmd>Git diff<cr>', { desc = 'Git diff' })
-  nmap('<leader>gp', '<cmd>Git push<cr>', { desc = 'Git push' })
-  nmap('<leader>gl', '<cmd>Git pull<cr>', { desc = 'Git pull' })
-  nmap('<leader>gs', '<cmd>Git status<cr>', { desc = 'Git status' })
-  nmap('<leader>gc', '<cmd>Git commit<cr>', { desc = 'Git commit' })
-  nmap('<leader>gC', '<cmd>Git commit --verbose<cr>', { desc = 'Git commit verbose' })
-  nmap('<leader>gb', '<cmd>GBrowse<cr>', { desc = 'Open in browser at hosting provider' })
-end
-
 return {
   'tpope/vim-fugitive',
-  cmd = { 'Git' },
-  config = config,
   dependencies = { 'tpope/vim-rhubarb' },
+  cmd = { 'Git' },
+  keys = {
+    { '<leader>gG', '<cmd>Git<cr>', desc = 'Git summary window' },
+    { '<leader>gV', '<cmd>vert Git<cr>', desc = 'Git summary window in vertical split' },
+    { '<leader>gT', '<cmd>tabnew | Git<cr><c-w>o<cr>', desc = 'Git summary window in new tab' },
+    { '<leader>gaa', '<cmd>Git add --all<cr>', desc = 'Git add all' },
+    { '<leader>gd', '<cmd>Git diff<cr>', desc = 'Git diff' },
+    { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git push' },
+    { '<leader>gl', '<cmd>Git pull<cr>', desc = 'Git pull' },
+    { '<leader>gs', '<cmd>Git status<cr>', desc = 'Git status' },
+    { '<leader>gc', '<cmd>Git commit<cr>', desc = 'Git commit' },
+    { '<leader>gC', '<cmd>Git commit --verbose<cr>', desc = 'Git commit verbose' },
+    { '<leader>gb', '<cmd>GBrowse<cr>', desc = 'Open in browser at hosting provider' },
+  },
 }
