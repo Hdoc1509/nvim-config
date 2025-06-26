ac2830115cbd640d190d73779221cd8dbfcb8081
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-26 12:19:32 -0500

    refactor(cmp): change mapping to jump to snippet placeholders
    also prevents overlap with `tabout.nvim`

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -27,18 +27,14 @@ local config = function()
       documentation = cmp_window_bordered,
     },
     mapping = cmp.mapping.preset.insert({
-      ['<Tab>'] = cmp.mapping(function(fallback)
-        if vim.fn['vsnip#jumpable'](1) == 1 then
-          feedkey('<Plug>(vsnip-jump-next)', '')
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
+      ['<C-h>'] = cmp.mapping(function()
         if vim.fn['vsnip#jumpable'](-1) == 1 then
           feedkey('<Plug>(vsnip-jump-prev)', '')
-        else
-          fallback()
+        end
+      end, { 'i', 's' }),
+      ['<C-l>'] = cmp.mapping(function()
+        if vim.fn['vsnip#jumpable'](1) == 1 then
+          feedkey('<Plug>(vsnip-jump-next)', '')
         end
       end, { 'i', 's' }),
       ['<C-j>'] = cmp.mapping(cmp.mapping.select_next_item()),
