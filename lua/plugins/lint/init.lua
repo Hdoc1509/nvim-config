776184128da6ad07154671b2784eb5795d1608da
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-27 10:28:03 -0500

    refactor(lint): simplify creation of autocommands for linters

--- lua/plugins/lint/init.lua
+++ lua/plugins/lint/init.lua
@@ -7,39 +7,30 @@ local config = function()
   ---@type string[]
   local aggressive_events = vim.fn.extendnew(normal_events, { 'InsertLeave', 'TextChanged' })
 
+  ---@type table<string, table<string, string | string[]>>
+  local linter_patterns = {
+    aggressive_events = {
+      actionlint = { '*.github/workflows/*.yml', '*.github/workflows/*.yaml' },
+      markdownlint = '*.md',
+      selene = '*.lua',
+      groovy_lint = { '*.gradle', '*.groovy' },
+    },
+  }
+
   lint.linters.groovy_lint = require('plugins.lint.npm-groovy-lint')
   lint.linters.markdownlint.parser = require('plugins.lint.markdownlint').parser
   lint.linters.selene.parser = require('plugins.lint.selene').parser
 
-  autocmd(aggressive_events, {
-    pattern = { '*.github/workflows/*.yml', '*.github/workflows/*.yaml' },
-    callback = function()
-      lint.try_lint('actionlint')
-    end,
-  })
-
-  autocmd(aggressive_events, {
-    pattern = '*.md',
-    callback = function()
-      lint.try_lint('markdownlint')
-    end,
-  })
+  for linter, pattern in pairs(linter_patterns.aggressive_events) do
+    autocmd(aggressive_events, {
+      pattern = pattern,
+      callback = function()
+        lint.try_lint(linter)
+      end,
+    })
+  end
 
   -- TODO: try to add `ts_query_ls lint` for query files in `query/` folder
-
-  autocmd(aggressive_events, {
-    pattern = '*.lua',
-    callback = function()
-      lint.try_lint('selene')
-    end,
-  })
-
-  autocmd(aggressive_events, {
-    pattern = { '*.gradle', '*.groovy' },
-    callback = function()
-      lint.try_lint('groovy_lint')
-    end,
-  })
 end
 
 return {
