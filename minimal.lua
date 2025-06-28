dad9315d88a2e6a1bd027e13d2276f7d0c236bb1
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-28 10:14:14 -0500

    Revert "refactor: move some files to `lua/user/` folder"
    This reverts commit 11b94b062b8beb8d55ea2f35c14b3a83cfc4c996.
    
    try to use the less subfolders as possible

--- minimal.lua
+++ minimal.lua
@@ -1,8 +1,8 @@
-require('user.globals')
-require('user.options')
-require('user.lazy_bootstrap')
+require('globals')
+require('options')
+require('lazy_bootstrap')
 require('lazy').setup({
   { import = 'plugins.lightline' },
   { import = 'plugins.colorscheme' },
 })
-require('user.maps')
+require('maps')
