f0449e4b081588031cf444e9bf205be5e25902c3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-28 11:29:12 -0500

    refactor(java): simplify custom `textDocument/publishDiagnostics` handler

--- lua/plugins/java/handlers.lua
+++ lua/plugins/java/handlers.lua
@@ -1,35 +1,35 @@
 local merge = require('utils').merge
 local default_handlers = require('plugins.lsp.handlers')
 
+---@param diagnostic Diagnostic
+---@param uri string
+local function is_diagnostic_ignored(diagnostic, uri)
+  if string.match(uri, 'hotbar%-keys') == nil then
+    return false
+  end
+
+  local message = diagnostic.message
+
+  return string.match(message, 'Constants cannot be resolved') ~= nil
+    or string.match(message, 'CommonClass cannot be resolved') ~= nil
+    or string.match(message, 'FabricPlatformHelper') ~= nil
+    or string.match(message, 'IPlatformHelper') ~= nil
+    or string.match(message, 'platform%.services cannot be resolved') ~= nil
+end
+
 local handlers = merge(default_handlers, {
   -- remove status from statusline
   ['language/status'] = function() end,
   -- disable some diagnostics
+  -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#publishDiagnosticsParams
+  -- https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#diagnostic
   ---@param result { diagnostics: Diagnostic[], uri: string }
   ['textDocument/publishDiagnostics'] = function(_, result, ctx, config)
     -- print(vim.inspect(result))
-    local hotbar_keys = string.match(result.uri, 'hotbar%-keys')
 
-    -- TODO: use vim.tbl_filter() as in parser of custom linters
-    if hotbar_keys ~= nil then
-      local idx = 1
-
-      while idx <= #result.diagnostics do
-        local message = result.diagnostics[idx].message
-
-        if
-          string.match(message, 'Constants cannot be resolved') ~= nil
-          or string.match(message, 'CommonClass cannot be resolved') ~= nil
-          or string.match(message, 'FabricPlatformHelper') ~= nil
-          or string.match(message, 'IPlatformHelper') ~= nil
-          or string.match(message, 'platform%.services cannot be resolved') ~= nil
-        then
-          table.remove(result.diagnostics, idx)
-        else
-          idx = idx + 1
-        end
-      end
-    end
+    result.diagnostics = vim.tbl_filter(function(diagnostic)
+      return not is_diagnostic_ignored(diagnostic, result.uri)
+    end, result.diagnostics)
 
     vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx, config)
   end,
