4600b4cbf80f9f5c93b73b0627cdee976819bb3b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-06-30 11:05:24 -0500

    fix(icons): prevent concatenation of `nil` when retrieving `hygen`-colored icon

--- lua/icons/hygen-compatible.lua
+++ lua/icons/hygen-compatible.lua
@@ -6,9 +6,14 @@ local function get_icon(filename)
   local extension = vim.fn.fnamemodify(filename, ':e')
 
   if extension == 'hygen' then
-    local _, _, target_extension = string.find(filename, '.*%.(%a+)(%.%a+)')
+    local _, _, subext, hygen_ext = string.find(filename, '.*%.(%a+)(%.%a+)')
+
+    if subext == nil or hygen_ext ~= '.hygen' then
+      return devicons.get_icon_color(filename, extension)
+    end
+
     local _, icon_color = devicons.get_icon_color('name.hygen', 'hygen')
-    local icon = devicons.get_icon('name.' .. target_extension, target_extension)
+    local icon = devicons.get_icon('name.' .. subext, subext)
 
     return icon, icon_color
   else
