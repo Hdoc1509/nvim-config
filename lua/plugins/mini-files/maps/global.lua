c0b6adeab0ee895b8fd34243a3b9816bac2a7f53
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-02 18:32:58 -0500

    feat(mini.files): disable mappings for `gitcommit` filetype

--- lua/plugins/mini-files/maps/global.lua
+++ lua/plugins/mini-files/maps/global.lua
@@ -3,7 +3,7 @@ local nmap = utils.nmap
 
 local M = {}
 
-local excluded_filetypes = { 'fugitive', 'minifiles', 'qf' }
+local excluded_filetypes = { 'fugitive', 'minifiles', 'qf', 'gitcommit' }
 
 ---@param onAllowed function
 local open_explorer = function(onAllowed)
