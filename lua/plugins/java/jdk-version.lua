c71599a40a0a974a1eca3a6e1c01c7396c37fe99
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-07 11:10:21 -0500

    refactor(java): simplify retrieval of jdk version

--- lua/plugins/java/jdk-version.lua
+++ lua/plugins/java/jdk-version.lua
@@ -8,20 +8,13 @@ M.get_from_gradle_properties = function(workspace_path)
 
   local gradle_properties = vim.fn.readfile(workspace_path .. '/gradle.properties', '', 20)
 
-  if gradle_properties == nil then
-    return nil
+  for _, line in ipairs(gradle_properties) do
+    local _, _, version = string.find(line, '^java_version=(%d+)')
+
+    if version ~= nil then
+      return version --[[@as string]]
+    end
   end
-
-  local line_with_version = vim.fn.filter(gradle_properties, function(_, line)
-    local _, _, _version = string.find(line, 'java_version=(%d+)')
-    return _version ~= nil
-  end)[1]
-
-  if line_with_version == nil then
-    return nil
-  end
-
-  return string.match(line_with_version, '%d+') --[[@as string]]
 end
 
 ---@param workspace_path string
@@ -32,22 +25,13 @@ M.get_from_sdkmanrc = function(workspace_path)
 
   local sdkmanrc = vim.fn.readfile(workspace_path .. '/.sdkmanrc', '', 20)
 
-  if sdkmanrc == nil then
-    return nil
+  for _, line in ipairs(sdkmanrc) do
+    local _, _, version = string.find(line, '^java=(.+)')
+
+    if version ~= nil then
+      return version --[[@as string]]
+    end
   end
-
-  local line_with_version = vim.fn.filter(sdkmanrc, function(_, line)
-    local _, _, version = string.find(line, 'java=(.+)')
-    return version ~= nil
-  end)[1]
-
-  if line_with_version == nil then
-    return nil
-  end
-
-  local _, _, version = string.find(line_with_version, 'java=(.+)')
-
-  return version --[[@as string]]
 end
 
 return M
