fc49cb37a86e264b62546873f6e756988b0a829a
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-12 10:50:33 -0500

    feat(highlight-colors): add custom ansi colors from my wezterm config

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -2,5 +2,14 @@ return {
   'brenoprata10/nvim-highlight-colors',
   opts = {
     exclude_filetypes = { 'lazy' },
+    enable_ansi = true,
+    custom_colors = {
+      -- light/bright colors from:
+      -- https://github.com/Hdoc1509/dotfiles/tree/master/wezterm/colors.lua
+      { label = '033%[1;31m', color = '#fe0100' },
+      { label = '033%[1;32m', color = '#29cc00' },
+      { label = '033%[1;34m', color = '#0066ff' },
+      { label = '033%[1;36m', color = '#00d5d5' },
+    },
   },
 }
