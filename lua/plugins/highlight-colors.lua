abe1aac578118fea427af9cf4cf68d9d15aa576a
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-12 18:58:41 -0500

    feat(highlight-colors): add support for `tput setaf` codes

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -13,6 +13,19 @@ return {
       { label = '033%[1;34m', color = '#0066ff' },
       { label = '033%[1;35m', color = '#cc00ff' },
       { label = '033%[1;36m', color = '#00d5d5' },
+      -- based on: https://www.ditig.com/256-colors-cheat-sheet
+      -- visual reference:
+      -- https://camo.githubusercontent.com/bb6e49c14a1925e7cd3537b6958b3d935de7516591a79e17ad1547ca0cf79313/68747470733a2f2f67756e732e6769746875622e696f2f787465726d2d636f6c6f722d7461626c652e76696d2f696d616765732f787465726d2d636f6c6f722d7461626c652d776974682d76697369626c652d7267622e706e67
+      -- closest to DiagnosticOk, String, @string
+      { label = 'tput setaf 34', color = '#00af00' },
+      -- closest to @variable.parameter
+      { label = 'tput setaf 75', color = '#5fafff' },
+      -- closest to DiagnosticInfo, Function, @function
+      { label = 'tput setaf 111', color = '#87afff' },
+      -- closest to DiagnosticWarn
+      { label = 'tput setaf 141', color = '#af87ff' },
+      -- closest to DiagnosticError, @function.builtin
+      { label = 'tput setaf 198', color = '#ff0087' },
     },
   },
 }
