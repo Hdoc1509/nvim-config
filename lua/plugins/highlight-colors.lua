ccd0ea2265ea10096749e751ea39436fba949501
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-12 10:57:38 -0500

    feat(highlight-colors): add yellow colors from my wezterm config

--- lua/plugins/highlight-colors.lua
+++ lua/plugins/highlight-colors.lua
@@ -8,6 +8,7 @@ return {
       -- https://github.com/Hdoc1509/dotfiles/tree/master/wezterm/colors.lua
       { label = '033%[1;31m', color = '#fe0100' },
       { label = '033%[1;32m', color = '#29cc00' },
+      { label = '033%[1;33m', color = '#feff00' },
       { label = '033%[1;34m', color = '#0066ff' },
       { label = '033%[1;36m', color = '#00d5d5' },
     },
