33b516588773ffa5b247fd34eb94e2e6801a1465
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-18 10:49:09 -0500

    feat(render-markdown): apply integration with `hygen.nvim`

--- lua/plugins/render-markdown.lua
+++ lua/plugins/render-markdown.lua
@@ -1,6 +1,10 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+    -- { dir = '~/dev/hygen.nvim' },
+    { 'Hdoc1509/hygen.nvim', version = '*' },
+  },
   ---@type render.md.UserConfig
   opts = {
     heading = {
@@ -19,24 +23,28 @@ return {
       sign = false,
       width = 'block',
       below = '',
-      above = '',
       language_border = '',
     },
-    ignore = function(bufnr)
-      local filename = vim.api.nvim_buf_get_name(bufnr)
-      local extension = vim.fn.fnamemodify(filename, ':e')
-
-      if extension == 'hygen' then
-        local _, _, subextension = string.find(filename, '.*%.(%a+)(%.%a+)')
-
-        return subextension ~= 'md'
-      else
-        return false
-      end
-    end,
     html = {
       comment = { conceal = false },
     },
   },
   ft = { 'markdown', 'markdown.mdx', 'hygen', 'gitcommit' },
+  config = function(_, opts)
+    local hygen_utils = require('hygen.utils')
+    local allowed_subext = { 'md', 'mdx' }
+
+    local ignore = function(bufnr)
+      local filename = vim.api.nvim_buf_get_name(bufnr)
+      local extension = vim.fn.fnamemodify(filename, ':e')
+
+      if extension == 'hygen' then
+        return not vim.tbl_contains(allowed_subext, hygen_utils.get_hygen_subext(filename))
+      else
+        return false
+      end
+    end
+
+    require('render-markdown').setup(vim.tbl_deep_extend('force', opts, { ignore = ignore }))
+  end,
 }
