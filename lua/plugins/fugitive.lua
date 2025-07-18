c143d8b302cb3d6f9dac7cfc0dcfea5dd94c4009
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-18 16:42:57 -0500

    fix(fugitive): remove unnecessary `<cr>` from mapping

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,7 +5,7 @@ return {
   keys = {
     { '<leader>gG', '<cmd>Git<cr>', desc = 'Git summary window' },
     { '<leader>gV', '<cmd>vert Git<cr>', desc = 'Git summary window in vertical split' },
-    { '<leader>gT', '<cmd>tabnew | Git<cr><c-w>o<cr>', desc = 'Git summary window in new tab' },
+    { '<leader>gT', '<cmd>tabnew | Git<cr><c-w>o', desc = 'Git summary window in new tab' },
     { '<leader>gaa', '<cmd>Git add --all<cr>', desc = 'Git add all' },
     { '<leader>gp', '<cmd>Git push<cr>', desc = 'Git push' },
     { '<leader>gl', '<cmd>Git pull<cr>', desc = 'Git pull' },
