2d65e9143655dc8522af557bb19d7471f1cdbb21
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-18 11:27:12 -0500

    fix(queries): apply injection offset of `githubaction` for `yaml`

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -24,7 +24,8 @@
   value: (block_node
     (block_scalar) @injection.content
     (#lua-match? @injection.content "${{")
-    (#set! injection.language "githubactions")))
+    (#set! injection.language "githubactions")
+    (#offset! @injection.content 0 1 0 0)))
 
 (block_mapping_pair
   key: (flow_node) @_key
