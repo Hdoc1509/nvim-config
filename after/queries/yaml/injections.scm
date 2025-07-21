4153b17f07a679da74ef77e9e428b06cf6be4bb3
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-21 18:36:18 -0500

    refactor(queries): unify `regex` injection for github actions files

--- after/queries/yaml/injections.scm
+++ after/queries/yaml/injections.scm
@@ -36,34 +36,32 @@
     (#set! injection.language "githubactions")))
 
 (block_mapping_pair
-  key: (flow_node) @_key
-  (#any-of? @_key "branches" "paths")
-  value: (block_node
-    (block_sequence
-      (block_sequence_item
-        (flow_node
-          [
-            (double_quote_scalar) @injection.content
-            (plain_scalar
-              (string_scalar) @injection.content)
-          ])))
-    (#set! injection.language "regex")))
-
-(block_mapping_pair
   key: (flow_node) @_on
   (#eq? @_on "on")
   value: (block_node
     (block_mapping
       (block_mapping_pair
-        key: (flow_node) @_push
-        (#eq? @_push "push")
+        key: (flow_node) @_event
+        (#any-of? @_event "push" "pull_request")
         value: (block_node
           (block_mapping
             (block_mapping_pair
-              key: (flow_node) @_tags
-              (#eq? @_tags "tags")
-              value: (flow_node
-                (flow_sequence
-                  (flow_node
-                    (double_quote_scalar) @injection.content))
-                (#set! injection.language "regex")))))))))
+              key: (flow_node) @_tags_paths
+              (#any-of? @_tags_paths "tags" "paths")
+              value: [
+                (flow_node
+                  (flow_sequence
+                    (flow_node
+                      (double_quote_scalar) @injection.content)))
+                (block_node
+                  (block_sequence
+                    (block_sequence_item
+                      (flow_node
+                        [
+                          (double_quote_scalar) @injection.content
+                          (plain_scalar
+                            (string_scalar) @injection.content)
+                        ]))))
+              ]
+              (#lua-match? @injection.content "%*")
+              (#set! injection.language "regex"))))))))
