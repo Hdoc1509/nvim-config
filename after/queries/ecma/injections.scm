3bfcc9fdf5c6a6670470e47499fb237451b23c56
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-21 19:12:29 -0500

    refactor(queries): unify `css` injection for javascript

--- after/queries/ecma/injections.scm
+++ after/queries/ecma/injections.scm
@@ -1,29 +1,20 @@
 ; extends
 
 ; === CSS injections ===
-; Element.matches()
 (call_expression
-  (member_expression
-    (property_identifier) @method
-    (#any-of? @method "matches" "querySelector" "querySelectorAll" "closest"))
+  [
+    (member_expression
+      (property_identifier) @method
+      (#any-of? @method "matches" "querySelector" "querySelectorAll" "closest"))
+    ; custom functions that I use
+    ((identifier) @function.name
+      (#any-of? @function.name "getElementBySelector" "getAllElementsBySelector"))
+  ]
   (arguments
     [
       (string
         (string_fragment) @injection.content)
       (template_string
         (string_fragment) @injection.content)
-    ]
-    (#set! injection.language "css")))
-
-; custom functions that I use
-(call_expression
-  ((identifier) @function.name
-    (#any-of? @function.name "getElementBySelector" "getAllElementsBySelector"))
-  (arguments
-    [
-      (string
-        (string_fragment) @injection.content)
-      (template_string
-        (string_fragment) @injection.content)
-    ]
-    (#set! injection.language "css")))
+    ])
+  (#set! injection.language "css"))
