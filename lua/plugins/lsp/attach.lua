ee402307fa3167bdca889090115e4abff8577b82
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-28 13:09:49 -0500

    feat(snacks): use `snacks.words` to show and jump to LSP references

--- lua/plugins/lsp/attach.lua
+++ lua/plugins/lsp/attach.lua
@@ -10,11 +10,8 @@ local lsp_log_enabled = false
 
 -- LspAttach from :https://github.com/neovim/nvim-lspconfig?tab=readme-ov-file#suggested-configuration
 local attach = function(ev)
-  local bufnr = ev.buf
-  local client = vim.lsp.get_client_by_id(ev.data.client_id)
-
   local buf_nmap = function(lhs, rhs, opts)
-    nmap(lhs, rhs, merge({ buffer = bufnr }, opts or {}))
+    nmap(lhs, rhs, merge({ buffer = ev.buf }, opts or {}))
   end
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
@@ -24,37 +21,12 @@ local attach = function(ev)
   buf_nmap('gO', lsp_buf.document_symbol) -- NOTE: not needed from nvim-0.11
   buf_nmap('J', lsp_buf.signature_help)
   buf_nmap('K', function()
-    hover_multi_lsp(bufnr)
+    hover_multi_lsp(ev.buf)
   end)
   buf_nmap('grt', lsp_buf.type_definition)
   buf_nmap('grn', lsp_buf.rename) -- NOTE: not needed from nvim-0.11
   buf_nmap('gra', lsp_buf.code_action) -- NOTE: not needed from nvim-0.11
   buf_nmap('grr', lsp_buf.references) -- NOTE: not needed from nvim-0.11
-
-  -- From: https://github.com/neovim/nvim-lspconfig/wiki/UI-Customization#highlight-symbol-under-cursor
-  if client.server_capabilities.documentHighlightProvider then
-    vim.cmd([[
-      hi! link LspReferenceRead Visual
-      hi! link LspReferenceText Visual
-      hi! link LspReferenceWrite Visual
-    ]])
-
-    vim.api.nvim_create_augroup('lsp_document_highlight', { clear = false })
-    vim.api.nvim_clear_autocmds({
-      buffer = bufnr,
-      group = 'lsp_document_highlight',
-    })
-    autocmd({ 'CursorHold', 'CursorHoldI' }, {
-      group = 'lsp_document_highlight',
-      buffer = bufnr,
-      callback = lsp_buf.document_highlight,
-    })
-    autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-      group = 'lsp_document_highlight',
-      buffer = bufnr,
-      callback = lsp_buf.clear_references,
-    })
-  end
 
   -- NOTE: you can create user commands for specific LSP server
   -- see :help lspconfig-configurations
--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,5 +4,22 @@ return {
   lazy = false,
   opts = {
     input = {},
+    words = {},
+  },
+  keys = {
+    {
+      ']r',
+      function()
+        Snacks.words.jump(1, true)
+      end,
+      desc = 'Got to next LSP reference',
+    },
+    {
+      '[r',
+      function()
+        Snacks.words.jump(-1, true)
+      end,
+      desc = 'Got to previous LSP reference',
+    },
   },
 }
