1f1c0ed6f15170d9352b0ffac974c574f8f1a978
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-07-28 11:01:30 -0500

    feat(diffview): update layout based on terminal width

--- lua/plugins/diffview.lua
+++ lua/plugins/diffview.lua
@@ -1,11 +1,52 @@
+local DEFAULT_DIFFOPT = 'internal,filler,closeoff'
+
+local function apply_dynamic_layout()
+  if vim.o.columns > 100 then
+    vim.o.diffopt = DEFAULT_DIFFOPT .. ',vertical'
+  else
+    vim.o.diffopt = DEFAULT_DIFFOPT .. ',horizontal'
+  end
+end
+
 return {
   'sindrets/diffview.nvim',
   cmd = { 'DiffviewFileHistory', 'DiffviewOpen' },
+  config = function()
+    require('diffview').setup({
+      view = {
+        default = { layout = -1 },
+        file_history = { layout = -1 },
+      },
+    })
+
+    vim.keymap.set('n', '<leader>df', function()
+      apply_dynamic_layout()
+
+      if vim.v.count > 0 then
+        vim.cmd('DiffviewFileHistory --max-count=' .. vim.v.count .. ' %')
+      else
+        vim.cmd('DiffviewFileHistory %')
+      end
+    end, { desc = 'Show file history' })
+
+    vim.keymap.set('n', '<leader>db', function()
+      apply_dynamic_layout()
+
+      if vim.v.count > 0 then
+        vim.cmd('DiffviewFileHistory --max-count=' .. vim.v.count)
+      else
+        vim.cmd('DiffviewFileHistory')
+      end
+    end, { desc = 'Show branch history' })
+
+    vim.keymap.set('n', '<leader>D', function()
+      apply_dynamic_layout()
+      vim.cmd('DiffviewOpen')
+    end, { desc = 'Show diffs of current index' })
+  end,
   keys = {
-    -- TODO: use vim.v.count to limit number of commits to show
-    -- TODO: call in horizontal split
-    { '<leader>df', '<cmd>DiffviewFileHistory %<cr>', desc = 'Show file history' },
-    { '<leader>db', '<cmd>DiffviewFileHistory<cr>', desc = 'Show branch history' },
-    { '<leader>D', '<cmd>DiffviewOpen', desc = 'Show diffs of current index' },
+    { '<leader>df', nil, desc = 'Show file history' },
+    { '<leader>db', nil, desc = 'Show branch history' },
+    { '<leader>D', nil, desc = 'Show diffs of current index' },
   },
 }
