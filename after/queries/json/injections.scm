c1b3f1a31a84c3c9c25a58d3eb46ce41a776c52a
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-08-05 10:22:51 -0500

    feat(queries): add `json` injections

---
+++ after/queries/json/injections.scm
@@ -0,0 +1,11 @@
+; extends
+
+(pair
+  key: (string
+    (string_content) @_key
+    (#eq? @_key "scripts"))
+  value: (object
+    (pair
+      value: (string
+        (string_content) @injection.content
+        (#set! injection.language "bash")))))
