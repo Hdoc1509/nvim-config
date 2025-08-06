cd7b95bc2349cc5cfab58ea6e23ba4d2056ccc7b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-08-06 12:06:13 -0500

    feat(queries/json): add injections for `wireit` object

--- after/queries/json/injections.scm
+++ after/queries/json/injections.scm
@@ -22,3 +22,43 @@
         (string
           (string_content) @injection.content
           (#set! injection.language "gitignore"))))))
+
+(pair
+  key: (string
+    (string_content) @_wireit
+    (#eq? @_wireit "wireit"))
+  value: (object
+    (pair
+      value: (object
+        [
+          (pair
+            key: (string
+              (string_content) @_command
+              (#eq? @_command "command"))
+            value: (string
+              (string_content) @injection.content))
+          (pair
+            key: (string
+              (string_content) @_dependencies
+              (#eq? @_dependencies "dependencies"))
+            value: (array
+              (string
+                (string_content) @injection.content)))
+        ]
+        (#set! injection.language "bash")))))
+
+(pair
+  key: (string
+    (string_content) @_wireit
+    (#eq? @_wireit "wireit"))
+  value: (object
+    (pair
+      value: (object
+        (pair
+          key: (string
+            (string_content) @_files
+            (#eq? @_files "files"))
+          value: (array
+            (string
+              (string_content) @injection.content
+              (#set! injection.language "gitignore"))))))))
