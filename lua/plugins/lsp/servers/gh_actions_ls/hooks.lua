8ecf91701cd75d4d2838a980d21f7e3127eb30c2
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-10-02 23:01:41 -0500

    perf(lsp/gh_actions_ls): reduce vim.fn.system() calls
    use `vim.notify` instead of `print`

--- lua/plugins/lsp/servers/gh_actions_ls/hooks.lua
+++ lua/plugins/lsp/servers/gh_actions_ls/hooks.lua
@@ -1,6 +1,7 @@
 local M = {}
 
 local github_api_url = 'https://api.github.com/repos/'
+local notify_title = 'gh_actions_ls'
 
 local function parse_repo_origin(origin)
   return vim.trim(origin:gsub('https://github%.com/', ''):gsub('%.git', ''))
@@ -13,15 +14,14 @@ local setup_repos = function(config)
     -- NOTE: maybe github cli will be needed to fetch private repos
     -- https://stackoverflow.com/questions/13902593/how-does-one-find-out-ones-own-repo-id/74529107
 
-    vim.fn.system({ 'git', 'branch' })
+    local remote_origin = vim.fn.system({ 'git', 'remote', 'get-url', 'origin' })
     if vim.v.shell_error > 0 then
-      print('gh_actions_ls: not in a git repository')
+      vim.notify('Not in a git repository', 'warning', { title = notify_title })
       return
     end
 
-    local remote_origin = vim.fn.system({ 'git', 'remote', 'get-url', 'origin' })
-    if vim.v.shell_error > 0 or remote_origin:match('github%.com') == nil then
-      print('gh_actions_ls: not a github repository')
+    if remote_origin:match('github%.com') == nil then
+      vim.notify('Not a Github repository', 'warning', { title = notify_title })
       return
     end
 
@@ -30,13 +30,13 @@ local setup_repos = function(config)
     local repo_json = vim.fn.system({ 'curl', '-s', github_api_url .. repo_full_name })
 
     if vim.v.shell_error > 0 then
-      print('gh_actions_ls: error while fetching repo data')
+      vim.notify('Error while fetching repo data', 'error', { title = notify_title })
       return
     end
 
     local repo_data = vim.json.decode(repo_json)
     if repo_data == nil then
-      print('gh_actions_ls: error while parsing json repo data')
+      vim.notify('Error while parsing json repo data', 'error', { title = notify_title })
       return
     end
 
