570328dad14a36679def723e6a5a13afcbc7e949
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-10-06 11:02:01 -0500

    refactor(ts_query_ls): improve readability of descriptions for custom directives

--- lua/plugins/lsp/servers/ts_query_ls/directives.lua
+++ lua/plugins/lsp/servers/ts_query_ls/directives.lua
@@ -4,7 +4,10 @@ return {
       { type = 'any', arity = 'required' },
       { type = 'string', arity = 'variadic' },
     },
-    description = 'Sets key/value metadata for a specific match or capture. Value is accessible as either `metadata[key]` (match specific) or `metadata[capture_id][key]` (capture specific).',
+    description = table.concat({
+      'Sets key/value metadata for a specific match or capture.',
+      'Value is accessible as either `metadata[key]` (match specific) or `metadata[capture_id][key]` (capture specific).',
+    }, '\n'),
   },
   offset = {
     parameters = {
@@ -14,7 +17,10 @@ return {
       { type = 'string', arity = 'required' },
       { type = 'string', arity = 'required' },
     },
-    description = 'Takes the range of the capture and applies an offset. This will generate a new range object for the captured node as `metadata[capture_id].range`.',
+    description = table.concat({
+      'Takes the range of the capture and applies an offset.',
+      'This will generate a new range object for the captured node as `metadata[capture_id].range`.',
+    }, '\n'),
   },
   ['inject-mdx-js'] = {
     parameters = {
