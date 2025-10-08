37105d1f094bc03f68abf2701cbd641b5713ab38
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-10-08 11:07:18 -0500

    refactor(treesitter): simplify config for custom predicates

--- lua/plugins/treesitter/predicates.lua
+++ lua/plugins/treesitter/predicates.lua
@@ -4,50 +4,36 @@
 local config_path = vim.fn.stdpath('config')
 local plugins_path = config_path .. '/lua/plugins'
 
----@class treesitter.PredicateConfig
----@field name string
----@field callback TSPredicate
----@field opts? boolean
+---@type table<string, TSPredicate>
+local new_predicates = {
+  ['is-conf-file?'] = function(_, _, bufnr)
+    return vim.bo[bufnr].filetype == 'conf'
+  end,
+  ['is-lazy-config-file?'] = function(_, _, bufnr)
+    local filename = vim.api.nvim_buf_get_name(bufnr)
+    if filename:match('%.lua$') == nil or filename:match(plugins_path .. '/init%.lua$') ~= nil then
+      return false
+    end
 
----@type treesitter.PredicateConfig[]
-local predicates = {
-  {
-    name = 'is-conf-file?',
-    callback = function(_, _, bufnr)
-      return vim.bo[bufnr].filetype == 'conf'
-    end,
-  },
-  {
-    name = 'is-lazy-config-file?',
-    callback = function(_, _, bufnr)
-      local filename = vim.api.nvim_buf_get_name(bufnr)
-      if filename:match('%.lua$') == nil or filename:match(plugins_path .. '/init%.lua$') ~= nil then
-        return false
-      end
+    return filename:match(plugins_path .. '/[%a-]+/init%.lua$') ~= nil
+      or filename:match(plugins_path .. '/[%a-]+%.lua$') ~= nil
+  end,
+  ['is-nvim-config-file?'] = function(_, _, bufnr, pred)
+    local target_filename = pred[2]
+    local filename = vim.api.nvim_buf_get_name(bufnr)
 
-      return filename:match(plugins_path .. '/[%a-]+/init%.lua$') ~= nil
-        or filename:match(plugins_path .. '/[%a-]+%.lua$') ~= nil
-    end,
-  },
-  {
-    name = 'is-nvim-config-file?',
-    callback = function(_, _, bufnr, pred)
-      local target_filename = pred[2]
-      local filename = vim.api.nvim_buf_get_name(bufnr)
+    if type(target_filename) ~= 'string' or target_filename == '' then
+      return filename:match(config_path) ~= nil
+    end
 
-      if type(target_filename) ~= 'string' or target_filename == '' then
-        return filename:match(config_path) ~= nil
-      end
-
-      return config_path .. '/' .. target_filename == vim.api.nvim_buf_get_name(bufnr)
-    end,
-  },
+    return config_path .. '/' .. target_filename == vim.api.nvim_buf_get_name(bufnr)
+  end,
 }
 
 return {
   setup = function()
-    for _, predicate in ipairs(predicates) do
-      vim.treesitter.query.add_predicate(predicate.name, predicate.callback, predicate.opts)
+    for name, handler in pairs(new_predicates) do
+      vim.treesitter.query.add_predicate(name, handler)
     end
   end,
 }
