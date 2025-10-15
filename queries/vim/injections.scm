0fafb7db7a5085eb66f55566cd977f4bd2c86bd8
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-10-15 18:46:20 -0500

    feat(queries/vim): add `vim` injection to `cdo` with `%s` command

--- queries/vim/injections.scm
+++ queries/vim/injections.scm
@@ -20,3 +20,12 @@
   (#offset! @injection.content 0 1 0 -1)
   (#is-nvim-config-file? "ftplugin/markdown.vim")
   (#set! injection.language "vim"))
+
+(unknown_builtin_statement
+  (unknown_command_name) @_command
+  (#eq? @_command "cdo")
+  (arguments
+    (command_argument) @injection.content)
+  (#lua-match? @injection.content "^%%s")
+  (#offset! @injection.content 0 1 0 0)
+  (#set! injection.language "vim"))
