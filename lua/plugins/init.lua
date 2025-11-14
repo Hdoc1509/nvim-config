79ab5a5f9ffd5edc2015d6aa01779795d6917adc
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-11-14 16:56:42 -0500

    fix(mini.move): setup correctly

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,4 +1,4 @@
 return {
-  { 'nvim-mini/mini.move', version = false },
+  { 'nvim-mini/mini.move', opts = {}, version = false },
   { 'OXY2DEV/helpview.nvim', lazy = false, version = '^2.1.2' },
 }
