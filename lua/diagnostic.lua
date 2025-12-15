36424a58a452e21b34eaa8a55944dd1648254c95
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-12-14 20:46:13 -0500

    refactor(diagnostic): remove `numhl` option
    bad contrast by using it with sethen/line-number-change-mode.nvim

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -3,7 +3,7 @@ local ICONS = require('icons.diagnostics')
 -- Change diagnostic signs
 for type, icon in pairs(ICONS) do
   local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+  vim.fn.sign_define(hl, { text = icon, texthl = hl })
 end
 
 vim.diagnostic.config({
