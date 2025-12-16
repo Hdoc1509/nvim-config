9b16c4f911dcf56f6f5a73e8fa35fe53d4e5cd9b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-12-16 10:41:52 -0500

    refactor(diagnostic): use `signs` option instead of `vim.fn.sign_define()`
    because of soft deprecation of `vim.fn.sign_define()`

--- lua/diagnostic.lua
+++ lua/diagnostic.lua
@@ -1,11 +1,18 @@
 local ICONS = require('icons.diagnostics')
 
+---@type vim.diagnostic.Opts.Signs
+local signs = { text = {}, linehl = {} }
+
 -- Change diagnostic signs
 for type, icon in pairs(ICONS) do
   local hl = 'DiagnosticSign' .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl })
+  local diagnostic_key = string.upper(type)
+
+  signs.text[diagnostic_key] = icon
+  signs.linehl[diagnostic_key] = hl
 end
 
 vim.diagnostic.config({
+  signs = signs,
   float = { source = true, border = 'rounded' },
 })
