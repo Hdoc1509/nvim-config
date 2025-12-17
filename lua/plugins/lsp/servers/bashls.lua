3a2696889cab773329fe97420c250c3b6602fccb
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-12-17 11:25:30 -0500

    feat(lsp): improve custom diagnostic handlers
    - better typing of params
    - same naming for params as `vim.lsp.diagnostic.on_publish_diagnostics()`

--- lua/plugins/lsp/servers/bashls.lua
+++ lua/plugins/lsp/servers/bashls.lua
@@ -9,15 +9,16 @@ end
 return {
   filetypes = { 'sh', 'zsh' },
   handlers = {
-    ---@param result { diagnostics: lsp.Diagnostic[], uri: string }
-    ['textDocument/publishDiagnostics'] = function(_, result, ctx)
-      -- print(vim.inspect(result))
+    ---@param params lsp.PublishDiagnosticsParams
+    ---@param ctx lsp.HandlerContext
+    ['textDocument/publishDiagnostics'] = function(_, params, ctx)
+      -- print(vim.inspect(params))
 
-      result.diagnostics = vim.tbl_filter(function(diagnostic)
-        return not is_diagnostic_ignored(diagnostic, result.uri)
-      end, result.diagnostics)
+      params.diagnostics = vim.tbl_filter(function(diagnostic)
+        return not is_diagnostic_ignored(diagnostic, params.uri)
+      end, params.diagnostics)
 
-      vim.lsp.diagnostic.on_publish_diagnostics(_, result, ctx)
+      vim.lsp.diagnostic.on_publish_diagnostics(_, params, ctx)
     end,
   },
 }
